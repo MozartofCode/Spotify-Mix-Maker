19b1993aeda7f63d7df423ff7440c9005a976f79
bb6363 <bb6363@g.rit.edu>
2023-12-26 19:40:34 -0500

    Created the frontend page for login/registeration and connected to the backend

--- backend/app.py
+++ backend/app.py
@@ -24,59 +24,25 @@ client_id = ""
 client_secret = ""
 
 # Credentials for MangoDB
-mongo_username = ""
-mongo_password = ""
-mongo_cluster = ""
+mongo_username = "newMangoUser"
+mongo_password = "MkqL4Dw0gOvQIC88"
+mongo_cluster = "spotify-mix-cluster"
 
+def connect_mongoDB():
+    
+    uri = "mongodb+srv://" + mongo_username + ":" + mongo_password + "@" + mongo_cluster + ".i73vml1.mongodb.net/?retryWrites=true&w=majority"
 
-uri = "mongodb+srv://" + mongo_username + ":" + mongo_password + "@" + mongo_cluster + ".i73vml1.mongodb.net/?retryWrites=true&w=majority"
+    # Create a new client and connect to the server
+    client = MongoClient(uri, tlsCAFile=certifi.where())
 
-# Create a new client and connect to the server
-client = MongoClient(uri, tlsCAFile=certifi.where())
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        return client
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-
-except Exception as e:
-    print(e)
-
-
-
-def initialization():
-
-    auth_url = 'https://accounts.spotify.com/api/token'
-
-    auth_headers = {
-        'Authorization': 'Basic ' + base64.b64encode((client_id + ':' + client_secret).encode()).decode('utf-8')
-    }
-
-    auth_data = {
-        'grant_type': 'client_credentials'
-    }
-
-    response = requests.post(auth_url, headers=auth_headers, data=auth_data)
-
-    if response.status_code == 200:
-        body = response.json()
-        token = body.get('access_token')
-        token_type = body.get('token_type')
-        expires_in = body.get('expires_in')
-        # print(f'Token: {token}')
-        # print(f'Token Type: {token_type}')
-        # print(f'Expires in: {expires_in}')
-        
-    else:
-        print(f'Error: {response.status_code}')
-        print(response.text)
-
-
-#initialization()
-
-
-
-
+    except Exception as e:
+        print(e)
 
 
 @app.route('/api/login', methods=['POST'])
@@ -91,6 +57,8 @@ def login():
 
     username = data['username']
     password = data['password']
+
+    client = connect_mongoDB()
 
     # Create or access a database
     db = client["users"]
@@ -113,6 +81,7 @@ def login():
 @app.route('/api/register', methods=['POST'])
 def register():
     
+    print("Received register request")
     # Extract username and password from the request body
     data = request.json
 
@@ -124,7 +93,10 @@ def register():
     password = data['password']
 
 
-    # Create or access a database
+    # Create or access a database   
+
+    client = connect_mongoDB()
+
     db = client["users"]
 
     # Create or access a collection
@@ -144,61 +116,70 @@ def register():
     return jsonify({'message': 'User registered successfully'})    
 
 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ## there has to be some sort of a search logic or like send a message to the user logic or smth?
 
 
 
+# def initialization():
 
-# Define a route to handle GET requests to '/'
-@app.route('/api/getPlaylist', methods=['GET'])
-def get_playlist(playlist_name):
-    return
+#     auth_url = 'https://accounts.spotify.com/api/token'
+
+#     auth_headers = {
+#         'Authorization': 'Basic ' + base64.b64encode((client_id + ':' + client_secret).encode()).decode('utf-8')
+#     }
+
+#     auth_data = {
+#         'grant_type': 'client_credentials'
+#     }
+
+#     response = requests.post(auth_url, headers=auth_headers, data=auth_data)
+
+#     if response.status_code == 200:
+#         body = response.json()
+#         token = body.get('access_token')
+#         token_type = body.get('token_type')
+#         expires_in = body.get('expires_in')
+#         # print(f'Token: {token}')
+#         # print(f'Token Type: {token_type}')
+#         # print(f'Expires in: {expires_in}')
+        
+#     else:
+#         print(f'Error: {response.status_code}')
+#         print(response.text)
+
+
+# #initialization()
+
+
+
+# # Define a route to handle GET requests to '/'
+# @app.route('/api/getPlaylist', methods=['GET'])
+# def get_playlist(playlist_name):
+#     return
 
 
 
 
 
-# Define a route to handle GET requests to '/'
-# Add this song
-@app.route('/api/swipeRight', methods=['GET'])
-def swipe_right():    
-    return
+# # Define a route to handle GET requests to '/'
+# # Add this song
+# @app.route('/api/swipeRight', methods=['GET'])
+# def swipe_right():    
+#     return
 
 
-# Define a route to handle GET requests to '/'
-# Don't add this song
-@app.route('/api/swipeLeft', methods=['GET'])
-def swipe_left():
-    return
+# # Define a route to handle GET requests to '/'
+# # Don't add this song
+# @app.route('/api/swipeLeft', methods=['GET'])
+# def swipe_left():
+#     return
 
 
-# Define a route to handle GET requests to '/'
-# Don't add this song
-@app.route('/api/mix', methods=['GET'])
-def make_mix():
-    return
+# # Define a route to handle GET requests to '/'
+# # Don't add this song
+# @app.route('/api/mix', methods=['GET'])
+# def make_mix():
+#     return
 
 
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleRegister = async () => {
+    // Make a POST request to register endpoint
+    const response = await fetch('http://localhost:5000/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.status === 400) {
+      alert("Username already exists! Try Logging-in");
+    }
+
+    else if (response.status === 404) {
+      alert("Error trying to register the user! Try again...");
+    }
+
+    else {
+      alert("Registeration successful!");
+    }
+    
+  };
+
+  const handleLogin = async () => {
+    // Make a POST request to login endpoint
+    const response = await fetch('http://localhost:5000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.status === 404) {
+      alert("User not found! Try again or register if you are a new user...");
+    }
+
+    else {
+      const data = await response.json();  
+      console.log(data);
+    }
+
+
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login Page</h1>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </div>
+      <div>
+        <button onClick={handleRegister}>Register</button>
+        <button onClick={handleLogin}>Login</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
